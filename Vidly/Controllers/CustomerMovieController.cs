3059ee2aa2e9affe9cd54623bf36077e638ac152
sojili <sudheesh009@gmail.com>
2016-08-15 15:40:05 -0400

    need to do the coding now

---
+++ Vidly/Controllers/CustomerMovieController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerMovieController : Controller
+    {
+        // GET: CustomerMovie
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+        //Action Result for Customer and List name is Customers 
+        public ActionResult Customer()
+            
+        {
+            var customer = new Customer();
+            List<Customer>customers = new List<Customer>()
+            {
+                new Customer() {Id=1,Name="jonny Walker" },
+                new Customer() {Id = 2, Name = "David " }
+            };
+            customer.Id = 1;
+
+            return View();
+        }
+    }
+}
